82a3a45f420aca092627313ded110ed48bf24c77
Jahidul Islam Rabby <rabbyjahidulislma5@gmail.com>
2026-05-14 21:39:26 +0600

    feat: Add public Home landing page with caregiver search, filter, and reviews

--- modern-stack/frontend/src/App.tsx
+++ modern-stack/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ModalProvider } from './components/ModalContext';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ClientDashboard from './pages/ClientDashboard';
@@ -11,12 +12,13 @@ function App() {
     <ModalProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/client" element={<ClientDashboard />} />
           <Route path="/caregiver" element={<CaregiverDashboard />} />
           <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </ModalProvider>
